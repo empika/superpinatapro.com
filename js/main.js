4fa20137107e3eed3dcdda573288227e72515bd3
Edd Parris <edd.parris@mediatonic.co.uk>
2014-03-05 16:12:40 +0000

    Piñata is just about right

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
     
     var canvas = document.getElementById('pinatacanvas');
     
-    var world = boxbox.createWorld(canvas, {gravity: {x: 0, y: 9}, scale: 100});
+    var world = boxbox.createWorld(canvas, {gravity: {x: 0, y: 9.8}, scale: 100, collisionOutlines: false});
     
     var thisEntity;
     var previousEntity;
@@ -14,9 +14,9 @@
         imageStretchToFit: true
     };
     
-    var x = 4.2;
+    var x = 6.2;
     
-    thisEntity = world.createEntity(linkTemplate, {x: x, y: -1, type: "static", friction: 2});
+    thisEntity = world.createEntity(linkTemplate, {x: x, y: -1, type: "static", friction: 2, angularDamping: 4});
     
     var y;
     for (y = 0; y < 4; y = y + 0.5) {
@@ -35,26 +35,39 @@
     previousEntity = thisEntity;
     thisEntity = world.createEntity({
         x: x,
-        y: y,
-        image: "/img/pinata_idle_big.png",
-        imageOffsetX: 0, //-0.5,
-        imageOffsetY: 0, //-0.6,
-        density: 2,
+        y: y, // - 0.2,
+        image: "/img/pinata_idle_huge.png",
+        imageOffsetX: -1,
+        imageOffsetY: -1,
+        density: 5,
         width: 1,
         height: 1,
-        imageStretchToFit: true
+        imageStretchToFit: false,
+        //friction: 0.5,
+        angularDamping: 4
     });
         
     world.createJoint(previousEntity, thisEntity, {
-        type: "revolute"
+        type: "revolute",
+        jointPositionOnEntity1: {x:0, y:.05}, 
+        jointPositionOnEntity2: {x:.5, y:.05}
     });
+
+    thisEntity.applyImpulse(-50, 40);
     
     var intervalID = window.setInterval(
         function() {
-            thisEntity.applyImpulse(50, 10) //Number(Math.random() * 90));
+            var random = Number(Math.random());
+            var direction = random > 0.5 ? Math.random() * 40 : Math.random() * -40;
+            //console.log(direction);
+            thisEntity.applyImpulse(-50, direction);
         }
-        , 5000
+        , 7000
     );
+
+    window.peck = function() {
+        thisEntity.applyImpulse(15, 1);
+    }
      
 })();
 
